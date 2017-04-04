26d88f27e59e765f5c0c355699fc52f87c310a26
lancaoxiangshe <wulantoo@live.com>
2017-04-04 17:43:57 +0800

    adjust image_uploader size and table show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,14 +14,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800,800]
+  process resize_to_fit: [800,1200]
 
   version :thumb do
-    process resize_to_fill: [200,200]
+    process resize_to_fill: [100,150]
   end
 
   version :medium do
-    process resize_to_fill: [400,400]
+    process resize_to_fill: [400,600]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
